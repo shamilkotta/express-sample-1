94cc9b45c2ed56f18343d63c5c150610dff0798b
shamilkotta <shamilkotta99@gmail.com>
2022-10-22 15:47:02 +0530

    server created

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const app = require('./app');
+
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+
+app.set("port", port);
+server.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${port}`);
+});
