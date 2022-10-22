677c609410691caf7ec73cf685e6f16cae81b2cb
shamilkotta <shamilkotta99@gmail.com>
2022-10-22 16:57:45 +0530

    middlewares  added

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const experss = require("express");
+const cookieParser = require("cookie-parser");
+const path = require("path")
+
 const indexRouter = require("./routes");
 const errorHandler = require("./middleware/errorHandling");
 
 const app = experss();
 
+// Parsers
+app.use(experss.json()); // for parsing application/json
+app.use(experss.urlencoded()); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser()); // coockie parser
+
+// serve form public folder as /static path
+app.use("/static", experss.static(path.join(__dirname, 'public')));
+
+// Routing
 app.use("/", indexRouter);
+
+// 404 & Error handling 
 app.use((req, res, next) => {
   next(new Error("Not found"));
 });
