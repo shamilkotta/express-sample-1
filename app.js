dd8b337e83bf5fc8ab6ba66896bd63b6bc3ff202
shamilkotta <shamilkotta99@gmail.com>
2022-10-22 17:10:23 +0530

    views engine added

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const experss = require("express");
 const cookieParser = require("cookie-parser");
-const path = require("path")
+const path = require("path");
 
 const indexRouter = require("./routes");
 const errorHandler = require("./middleware/errorHandling");
 
 const app = experss();
+
+// Setting view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 // Parsers
 app.use(experss.json()); // for parsing application/json
@@ -13,12 +17,12 @@ app.use(experss.urlencoded()); // for parsing application/x-www-form-urlencoded
 app.use(cookieParser()); // coockie parser
 
 // serve form public folder as /static path
-app.use("/static", experss.static(path.join(__dirname, 'public')));
+app.use("/static", experss.static(path.join(__dirname, "public")));
 
 // Routing
 app.use("/", indexRouter);
 
-// 404 & Error handling 
+// 404 & Error handling
 app.use((req, res, next) => {
   next(new Error("Not found"));
 });
